daf31c5db2dd0ed42153e347010e2edcf24167a2
Jamal Shahverdiev <jamal.shahverdiev@gmail.com>
2016-05-19 21:53:12 +0500

    last update for long description

--- writelongdescfromcdp.py
+++ writelongdescfromcdp.py
@@ -5,7 +5,9 @@ import getpass
 import os
 import sys
 
-ciscoips = ['10.47.32.3', '10.47.32.4', '10.47.32.5']
+allips = open('iplist')
+ciscoips = allips.read().splitlines()
+allips.close()
 
 print("""
         Write CDP neighbor hostname and portid description to the local interface using SSH...
@@ -60,14 +62,13 @@ def write_descr(ip, username, password, enable_secret, localint, descr):
     rt = ssh_connection.send_command("conf t", delay_factor=0)
     rt = ssh_connection.send_command("int "+localint, delay_factor=0)
     rt = ssh_connection.send_command("description "+descr, delay_factor=0)
+    rt = ssh_connection.send_command("do write", delay_factor=0)
     ssh_connection.disconnect()
 
 for ip in ciscoips:
     get_cdp_neighbors(ip, username, password, enable_secret)
     ctfls = os.popen('cat temps/'+ip+' | grep / | wc -l').read()
-
     for i in range(1, int(ctfls)+1):
         descr = os.popen('cat temps/'+ip+' | grep -A 1 "bvim.lan" | awk \'NR%2{printf $0" ";next;}1\' | tail -n '+str(i)+' | awk -F\' \' \'BEGIN{OFS="-"} {print $1,$8,$9}\'').readline().strip()
         localint = os.popen('cat temps/'+ip+'| grep -A 1 "bvim.lan" | awk \'NR%2{printf $0" ";next;}1\' | tail -n '+str(i)+' | awk \'{print $2,$3}\'').readline().strip()
         write_descr(ip, username, password, enable_secret, localint, descr)
-
